a8b17991428c7b9e5d64121b6f166799b20483ad
zekri <marcoz@MacBook-Pro-de-zekri.local>
2025-04-09 00:34:42 +0200

    Register admin blueprint in main.py

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from flask import Flask
 from wallet_route import wallet_bp
 from guess_route import guess_bp
 from dotenv import load_dotenv
+from admin_route import admin_bp
+
 
 load_dotenv()
 
 app = Flask(__name__)
 app.register_blueprint(wallet_bp, url_prefix="/")
 app.register_blueprint(guess_bp, url_prefix="/")
+app.register_blueprint(admin_bp)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
