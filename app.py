2196c81ed9538395c18b4e4d9b2f5594ecb6f143
zekri <marcoz@MacBook-Pro-de-zekri.local>
2025-04-10 15:11:37 +0200

    Fix: define app before registering blueprints

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask
 from admin_route import admin_bp
 from guess_route import guess_bp
-app.register_blueprint(guess_bp)
 
-app = Flask(__name__)
+app = Flask(__name__) 
+
 app.register_blueprint(admin_bp)
+app.register_blueprint(guess_bp)
 
 @app.route("/")
 def index():
-    return "Bienvenue sur l'API Passworld 🧠🔐"
+    return "Welcome to the Passworld API 🧠🔐"
 
 if __name__ == "__main__":
     app.run(debug=True)
